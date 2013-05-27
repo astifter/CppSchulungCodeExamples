945d37b834b75452b83fd1ca30253303a24f1514
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-27 15:40:43 +0200

    Added overloaded operator and class names.

--- SimpleClass/SimpleClass.cpp
+++ SimpleClass/SimpleClass.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 
-
 class SimpleClass {
+  private:
+    std::string name;
   public:
-    SimpleClass() {
-        std::cout << "SimpleClass() called." << std::endl;
+    SimpleClass() : name("default") {
+        std::cout << "SimpleClass() named \"" << name << "\" called." << std::endl;
+    }
+
+    SimpleClass(std::string n) : name(n) {
+        std::cout << "SimpleClass(std::string n) named \"" << name << "\" called." << std::endl;
     }
 
     ~SimpleClass() {
-        std::cout << "~SimpleClass() called." << std::endl;
+        std::cout << "~SimpleClass() for \""<<name<<"\"called." << std::endl;
     }
+
+    std::string getName(void) {
+        return this->name;
+    }
+
+    friend std::ostream & operator<< (std::ostream & other, SimpleClass & c);
 };
+
+std::ostream & operator<< (std::ostream & other, SimpleClass & c) {
+    return (other << c.name);
+}
 
 int main (int argc, char** argv) {
     std::cout << "main() called." << std::endl;
-    SimpleClass *c = new SimpleClass();
+    SimpleClass *d = new SimpleClass();
+    SimpleClass c = SimpleClass("c");
     
-    delete c;
+    std::cout << "object d: " << *d << std::endl;
+    std::cout << "object c: " << c << std::endl;
+
+    delete d;
     std::cout << "main() exiting." << std::endl;
     return 0;
 }
