13f9160332bc6dcab014ae6e44b48c8a0a4b36e3
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-27 14:39:46 +0200

    Added endl, removed memory leak.

--- SimpleClass/SimpleClass.cpp
+++ SimpleClass/SimpleClass.cpp
@@ -4,18 +4,19 @@
 class SimpleClass {
   public:
     SimpleClass() {
-        std::cout << "SimpleClass() called.";
+        std::cout << "SimpleClass() called." << std::endl;
     }
 
     ~SimpleClass() {
-        std::cout << "~SimpleClass() called.";
+        std::cout << "~SimpleClass() called." << std::endl;
     }
 };
 
 int main (int argc, char** argv) {
-    std::cout << "main() called.";
+    std::cout << "main() called." << std::endl;
     SimpleClass *c = new SimpleClass();
     
-    std::cout << "main() exiting.";
+    delete c;
+    std::cout << "main() exiting." << std::endl;
     return 0;
 }
