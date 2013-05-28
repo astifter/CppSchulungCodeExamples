561a95782d3602513220a52d73e3a27fd1b71473
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-28 16:24:48 +0200

    Implemented first version of composite.

---
+++ Composite/main.cpp
@@ -0,0 +1,16 @@
+#include "Circle"
+#include "Rectangle"
+#include <iostream>
+#include "ComplexShape"
+
+int main (int argc, char** argv) {
+    Rectangle<float> r(5,4);
+    Circle<float> c(5);
+    std::cout << r.str() << ", area: " << r.area() << std::endl;
+    std::cout << c.str() << ", area: " << c.area() << std::endl;
+
+    ComplexShape<float> cs;
+    cs.addShape(&r);
+    cs.addShape(&c);
+    std::cout << cs.str() << ", area: " << cs.area() << std::endl;
+}
