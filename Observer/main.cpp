d2acce26919c84fa99b3c29e0f68e5adc39a1361
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-28 14:13:18 +0200

    Added FastConsoleCallback.

--- Observer/main.cpp
+++ Observer/main.cpp
@@ -1,8 +1,15 @@
 #include "Generator"
+#include "FastConsoleCallback"
 #include "ConsoleCallback"
 
 int main ( int argc, char** argv ) {
-    ConsoleCallback cb = ConsoleCallback();
-    Generator g = Generator(cb);
+    ICallback *cb;
+    if (argc > 1 && std::string(argv[1]) == "-f") {
+        cb = new FastConsoleCallback();
+    } else {
+        cb = new ConsoleCallback();
+    }
+    Generator g = Generator(*cb);
     g.generate();
+    delete cb;
 }
