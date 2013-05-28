c7f221f330f910bf8c020ea6a68b8139d7ca9737
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-28 09:28:29 +0200

    Moved vector3f to templated class.

--- vector3/vector3.cpp
+++ vector3/vector3.cpp
@@ -4,86 +4,114 @@
 #define _DEBUG
 #define _CHECK 0
 
-class vector3f {
+template<typename T>
+class vector3;
+
+template<typename T>
+vector3<T> operator*(const vector3<T>& lhs, const T& rhs);
+template<typename T>
+vector3<T> operator/(const vector3<T>& lhs, const T& rhs);
+template<typename T>
+vector3<T> operator*(const T& lhs, const vector3<T>& rhs);
+template<typename T>
+vector3<T> operator/(const T& lhs, const vector3<T>& rhs);
+template<typename T>
+std::ostream& operator<< (std::ostream& other, const vector3<T>& c);
+
+template<typename T>
+class vector3 {
     private:
-        float x, y, z;
+        T x, y, z;
 
     public:
-        vector3f() : x(0), y(0), z(0) {
+        vector3() : x(0), y(0), z(0) {
 #ifdef _DEBUG
             std::cout << *this << " created" << std::endl;
 #endif
         }
-        vector3f(float _x, float _y, float _z): x(_x), y(_y), z(_z) {
+        vector3(T _x, T _y, T _z): x(_x), y(_y), z(_z) {
 #ifdef _DEBUG
             std::cout << *this << " created" << std::endl;
 #endif
         }
-        vector3f(const vector3f& other): x(other.x), y(other.y), z(other.z) {
+        vector3(const vector3<T>& other): x(other.x), y(other.y), z(other.z) {
 #ifdef _DEBUG
             std::cout << other << " copied to " << *this << std::endl;
 #endif
         }
 
-        vector3f& operator=(const vector3f & rhs) {
+        vector3<T>& operator=(const vector3<T> & rhs) {
 #ifdef _DEBUG
-            std::cout << "vector3f: entering operator= for " << *this << " with rhs " << rhs << std::endl;
+            std::cout << "vector3<T>: entering operator= for " << *this << " with rhs " << rhs << std::endl;
 #endif
             x = rhs.x; y = rhs.y; z = rhs.z;
         }
 
-        bool operator==(const vector3f & rhs) {
+        bool operator==(const vector3<T> & rhs) {
 #ifdef _DEBUG
-            std::cout << "vector3f: entering operator== for " << *this << " with rhs " << rhs << std::endl;
+            std::cout << "vector3<T>: entering operator== for " << *this << " with rhs " << rhs << std::endl;
 #endif
             return (x == rhs.x) && (y = rhs.y) && (z == rhs.z);
         }
-        bool operator!=(const vector3f & rhs) {
+        bool operator!=(const vector3<T> & rhs) {
             return !(*this == rhs);
         }
 
-        vector3f& operator*=(const float rhs) {
+        vector3<T>& operator*=(const T rhs) {
 #ifdef _DEBUG
-            std::cout << "vector3f: entering operator*= for " << *this << " with rhs " << rhs << std::endl;
+            std::cout << "vector3<T>: entering operator*= for " << *this << " with rhs " << rhs << std::endl;
 #endif
             x *= rhs; y *= rhs; z *= rhs;
             return *this;
         }
-        vector3f& operator/=(const float rhs) {
+        vector3<T>& operator/=(const T rhs) {
             return (*this)*=(1/rhs);
         }
 
 #ifdef _DEBUG
-        ~vector3f() {
+        ~vector3<T>() {
             std::cout << *this << " destroyed" << std::endl;
         }
 #endif
 
-        friend std::ostream& operator<< (std::ostream& other, const vector3f& v);
-        friend vector3f operator*(const vector3f& lhs, const float& rhs);
+        friend std::ostream& operator<< <> (std::ostream& other, const vector3<T>& v);
+        friend vector3<T> operator* <> (const vector3<T>& lhs, const T& rhs);
 };
 
-vector3f operator*(const vector3f& lhs, const float& rhs) {
+template<typename T>
+vector3<T> operator*(const vector3<T>& lhs, const T& rhs) {
 #ifdef _DEBUG
-    std::cout << "vector3f: entering operator* with " << lhs << " and rhs " << rhs << std::endl;
+    std::cout << "vector3<T>: entering operator* with " << lhs << " and rhs " << rhs << std::endl;
 #endif
-    vector3f nv = vector3f(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
+    vector3<T> nv = vector3<T>(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
     return nv;
 }
-vector3f operator/(const vector3f& lhs, const float& rhs) {
+
+template<typename T>
+vector3<T> operator/(const vector3<T>& lhs, const T& rhs) {
     return lhs*(1/rhs);
 }
 
-vector3f operator*(const float& lhs, const vector3f& rhs) {
+template<typename T>
+vector3<T> operator*(const T& lhs, const vector3<T>& rhs) {
     return rhs * lhs;
 }
-vector3f operator/(const float& lhs, const vector3f& rhs) {
+
+template<typename T>
+vector3<T> operator/(const T& lhs, const vector3<T>& rhs) {
     return rhs / lhs;
 }
 
-std::ostream& operator<< (std::ostream& other, const vector3f& c) {
-    return (other << "vector3f: [" << &c << "](x:" << c.x << ", y:" << c.y << ", z:" << c.z << ")");
+template<typename T>
+std::ostream& operator<< (std::ostream& other, const vector3<T>& c) {
+#ifdef _DEBUG
+    return (other << "vector3<T>: [" << &c << "](x:" << c.x << ", y:" << c.y << ", z:" << c.z << ")");
+#else
+    return (other << "(x:" << c.x << ", y:" << c.y << ", z:" << c.z << ")");
+#endif
 }
+
+typedef vector3<float> vector3f;
 
 int main (int argc, char** argv) {
     std::cout << "--- creating v" << std::endl;
@@ -91,15 +119,15 @@ int main (int argc, char** argv) {
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(1,2,4)) { throw new std::exception(); }
     std::cout << "--- creating n = v * 8.9" << std::endl;
-    vector3f n = 8.9 * v;
+    vector3f n = 8.9f * v;
     std::cout << "n: " << n << std::endl;
     if (_CHECK) if (n != vector3f(1*8.9,2*8.9,4*8.9)) { throw new std::exception(); }
     std::cout << "--- creating v = (v*2)" << std::endl;
-    v = (v*2);
+    v = (v*2.0f);
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(2,4,8)) { throw new std::exception(); }
     std::cout << "--- creating v = (2*v)" << std::endl;
-    v = (2*v);
+    v = (2.0f*v);
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(4,8,16)) { throw new std::exception(); }
     std::cout << "--- multipying v with 2" << std::endl;
@@ -107,11 +135,11 @@ int main (int argc, char** argv) {
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(8,16,32)) { throw new std::exception(); }
     std::cout << "--- creating v = (v/2)" << std::endl;
-    v = (v/2);
+    v = (v/2.0f);
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(4,8,16)) { throw new std::exception(); }
     std::cout << "--- creating v = (2/v)" << std::endl;
-    v = (2/v);
+    v = (2.0f/v);
     std::cout << "v: " << v << std::endl;
     if (_CHECK) if (v != vector3f(2,4,8)) { throw new std::exception(); }
     std::cout << "--- multipying v with 2" << std::endl;
