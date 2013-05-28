517046e72ada39ea896c412585ef57d239c589e7
Andreas Neustifter <andreas.neustifter@kapsch.net>
2013-05-28 11:18:49 +0200

    Implemented a stray animal home.

---
+++ inheritance/inheritance.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <exception>
+
+class Pet {
+    std::string name;
+  public:
+    Pet(const std::string & n) : name(n) {}
+
+    std::string getName() {
+        return name;
+    }
+    
+    virtual std::string speak() = 0;
+};
+
+class Cat : public Pet {
+  public:
+    Cat(const std::string & n) : Pet(n) {}
+
+    std::string speak() {
+        return "Meow, my name is " + getName() + ".";
+    }
+};
+
+class Dog : public Pet {
+  public:
+    Dog(const std::string & n) : Pet(n) {}
+
+    std::string speak() {
+        return "Woof, me name i' " + getName() + ", woff!";
+    }
+};
+
+class GermanShepard : public Dog {
+  public:
+    GermanShepard(const std::string & n) : Dog(n) {}
+
+    std::string speak() {
+        return "Woof, mein Name ist " + getName() + ". Jawoll.";
+    }
+};
+
+class StupidPoodle : public Dog {
+  public:
+    StupidPoodle(const std::string & n) : Dog(n) {}
+};
+
+void LetPetsSpeak(Pet& p) {
+    std::cout << p.speak() << std::endl;
+}
+
+int main (int argc, char** argv) {
+    //Pet p("pet");
+    Cat c("Herbert");
+    LetPetsSpeak(c);
+    Dog d("Carlo");
+    LetPetsSpeak(d);
+    GermanShepard g("Fritz");
+    LetPetsSpeak(g);
+    StupidPoodle p("Woofy");
+    LetPetsSpeak(p);
+    return 0;
+}
